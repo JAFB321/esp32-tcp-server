3ebe8b8dc42a4e0895619d30baca34d5dd26c642
jafb321 <jafb321@gmail.com>
2022-02-18 23:09:55 -0700

    feat: basic tcp client on nodejs

---
+++ client.js
@@ -0,0 +1,24 @@
+const {Socket} = require('net');
+
+const port = '22333';
+const host = '192.168.1.70';
+
+const client = new Socket();
+
+client.on('data', data => {
+    console.log(data.toString());
+});
+
+client.on('end', () => {
+    console.log('Connection ended');
+});
+
+client.on('error', err => {
+    console.log(err);
+});
+
+client.connect({port, host}, () => {
+    console.log('Connected to the server');
+    client.write('Hello!!!! \r');
+});
+
